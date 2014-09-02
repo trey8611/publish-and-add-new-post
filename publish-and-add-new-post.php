d9f7053d9e9a46885c97623b707315c6b8155624
Trey M <trey8611@gmail.com>
2014-09-02 17:01:39 -0400

    Removed a comment line.

--- publish-and-add-new-post.php
+++ publish-and-add-new-post.php
@@ -25,7 +25,6 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
     */
 if ( !class_exists( 'TPSandAN' ) ) {
-	// TPSandAN = ThemePulse Save and Add New
 	class TPSandAN {
 		
 		public function __construct() {
